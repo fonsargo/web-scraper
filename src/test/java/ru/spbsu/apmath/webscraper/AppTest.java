b1d588e256f4c75caee43e971ae8d56474f20235
Afonin Sergey <hrundelb@yandex.ru>
2014-08-07 13:49:59 +0400

    initial commit

---
+++ src/test/java/ru/spbsu/apmath/webscraper/AppTest.java
@@ -0,0 +1,17 @@
+package ru.spbsu.apmath.webscraper;
+
+import org.junit.Test;
+
+import javax.swing.text.BadLocationException;
+import java.io.IOException;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void generalTest() throws IOException, BadLocationException {
+
+    }
+}
